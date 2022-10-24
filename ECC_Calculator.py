3b2931aa97df478c5b4c7a71a4276d8acefe7c11
Sagnik Haldar <sagnikh2@gmail.com>
2022-10-24 12:12:52 +0530

    comments

--- ECC_Calculator.py
+++ ECC_Calculator.py
@@ -1,3 +1,5 @@
+#montgomery curve : By^2 = x^3 + Ax^2 + x
+
 import gmpy2
 
 #gets us the next prime if number isn't prime
@@ -5,6 +7,7 @@ def getPrime(number):
 	if gmpy2.is_prime(number):
 		return number
 	else:
+		print("{} isn't a prime so we consider the next prime for calculations".format(number))
 		return gmpy2.next_prime(number)
 
 #fast modular exponentiation
@@ -21,66 +24,69 @@ def modular_pow(base, exponent, modulus):
 def euler(quadraticResidue, prime):
 	return modular_pow(quadraticResidue, gmpy2.f_div(prime+1, 4), prime)
 
-def findQR(a, b, x, p):
-    return gmpy2.f_mod((x*x*x + a*x*x + x) // b, p)
+#y^2 = m (mod p); here m is the function of x
+def findM(a, b, x, p):
+		return gmpy2.f_mod((x*x*x + a*x*x + x) // b, p)
 
 def findQRForSW(a, b, x, p):
-    return gmpy2.f_mod((x*x*x + a*x + b), p)
+		return gmpy2.f_mod((x*x*x + a*x + b), p)
 
 def legendre(a, p):
-    return pow(a, (p - 1) // 2, p)
+		return modular_pow(a, (p - 1) // 2, p)
 
 def tonelli(n, p):
-    assert legendre(n, p) == 1, "not a square (mod p)"
-    q = p - 1
-    s = 0
-    while q % 2 == 0:
-        q //= 2
-        s += 1
-    if s == 1:
-        return pow(n, (p + 1) // 4, p)
-    for z in range(2, p):
-        if p - 1 == legendre(z, p):
-            break
-    c = pow(z, q, p)
-    r = pow(n, (q + 1) // 2, p)
-    t = pow(n, q, p)
-    m = s
-    t2 = 0
-    while (t - 1) % p != 0:
-        t2 = (t * t) % p
-        for i in range(1, m):
-            if (t2 - 1) % p == 0:
-                break
-            t2 = (t2 * t2) % p
-        b = pow(c, 1 << (m - i - 1), p)
-        r = (r * b) % p
-        c = (b * b) % p
-        t = (t * c) % p
-        m = i
-    return r
+		assert legendre(n, p) == 1, "not a square (mod p)"
+		q = p - 1
+		s = 0
+		while q % 2 == 0:
+				q //= 2
+				s += 1
+		if s == 1:
+				return pow(n, (p + 1) // 4, p)
+		for z in range(2, p):
+				if p - 1 == legendre(z, p):
+						break
+		c = pow(z, q, p)
+		r = pow(n, (q + 1) // 2, p)
+		t = pow(n, q, p)
+		m = s
+		t2 = 0
+		while (t - 1) % p != 0:
+				t2 = (t * t) % p
+				for i in range(1, m):
+						if (t2 - 1) % p == 0:
+								break
+						t2 = (t2 * t2) % p
+				b = pow(c, 1 << (m - i - 1), p)
+				r = (r * b) % p
+				c = (b * b) % p
+				t = (t * c) % p
+				m = i
+		return r
 
 def addPoint(x, m, p):
-    if gmpy2.f_mod(p, 4) == 3:
-      y = euler(m, p)
-    else:
-      y = tonelli(m, p)
+		if gmpy2.f_mod(p, 4) == 3:      
+			y = euler(m, p)
+		else:
+			y = tonelli(m, p)
 
-    print(x, y)
-    print(x, p - y)
+		print(x, y)
+		print(x, p - y)
 
 def findPoint(a, b, p):
-    for i in range(p):
-        # m = findQR(a, b, i, p)
-        m = findQRForSW(a, b, i, p)
-        # qr = (pow(m, (p - 1) // 2)) % p
-        quadraticResidue = modular_pow(m, gmpy2.f_div(p-1, 2), p)
-        if quadraticResidue == 1:
-            addPoint(i, m, p)
-        elif quadraticResidue == 0:
-            print('Single point solution: ', i, 0)
+		p = getPrime(p)
+		for i in range(p):
+				m = findM(a, b, i, p)
+				# m = findQRForSW(a, b, i, p)
+				# qr = (pow(m, (p - 1) // 2)) % p
+				
+				quadraticResidue = legendre(m, p)
+				if quadraticResidue == 1:
+						addPoint(i, m, p)
+				elif quadraticResidue == 0:
+						print('Single point solution: ', i, 0)
 
 def driver():
-    findPoint(2, 3, 13)
+		findPoint(5, 7, 10)
 
 driver()
